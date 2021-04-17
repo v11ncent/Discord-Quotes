070d55e1e8a4a6f167a6382eeed7210376ee59fa
Vince <45806875+vince1444@users.noreply.github.com>
2021-04-17 01:41:00 -0400

    Added basic functionality

---
+++ root/frontend/src/App.js
@@ -0,0 +1,23 @@
+import img from './amangus.PNG'
+
+function App() {
+  return (
+    <div className='parent-grid-container'>
+      <div className='grid-container'>
+        <div className='item-a'>
+          <img src={img} className='imposter' alt="amang us"></img>
+        </div>
+        <div className='item-b'>
+         
+        </div>
+        <div className='item-c'>
+
+        </div>
+        <div className='item-d'>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
